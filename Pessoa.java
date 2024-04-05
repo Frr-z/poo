fe96cb1e00598565d73907c697343d14435d1bc7
Frr-z <daniel@celebrar.fot.br>
2024-04-05 08:25:54 -0300

    third

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa{
+
+    public String nome;
+    public int idade;
+
+    public static void main(String[] args){
+    }
+
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
---
+++ Requirer.java
@@ -0,0 +1,9 @@
+public class Requirer {
+
+    public static void main(String[] args) {
+        Pessoa Pessoa1 = new Pessoa("Daniel", 16);
+        Pessoa Pessoa2 = new Pessoa("Antonio", 16);
+    
+        System.out.println("Pessoa 1: " + Pessoa1.nome +", "+ Pessoa1.idade + " anos" + "; Pessoa 2: " + Pessoa2.nome +", "+ Pessoa2.idade + " anos.");   
+    }
+}
